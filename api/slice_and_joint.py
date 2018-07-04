0436adfb1ad2fe55e6a831668b810192a76af570
supercoderhawk <supercoderhawk@gmail.com>
2018-07-04 15:29:41 +0800

    add codes

--- api/slice_and_joint.py
+++ api/slice_and_joint.py
@@ -1,5 +1,11 @@
 import tensorflow as tf
 
+class SliceAndJoint(object):
+  def __init__(self):
+    self.sess = tf.Session()
+
+
+
 sess = tf.Session()
 params = tf.constant([6, 3, 4, 1, 5, 9, 10])
 params2 = tf.constant([[1,2,3],[4,5,6],[7,8,9]])
